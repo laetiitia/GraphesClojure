d953a553e01b4fb2932614f7b8d4a979b900c064
laetitia.pham <laetitia.pham@etu.upmc.fr>
2019-03-20 21:57:44 +0100

    gen-alea

--- src/graphclj/graph.clj
+++ src/graphclj/graph.clj
@@ -10,7 +10,7 @@
 ;; Generate a graph from the lines
 (defn gen-graph [lines]
     "Returns a hashmap contating the graph"
-    (loop [v (map intVect lines), hmap (makeGraph lines)]
+    (loop [v (map intVect lines), hmap (makeGraph (reduce concat (map intVect lines)))]
       (if (seq v)
           (recur (rest v) (link (first v) hmap))
           hmap)))
@@ -18,17 +18,17 @@
 ;;Add link between the couple in v
 (defn link [v hmap]
   (if (= (first v) (second v))
-    hmap 
+    hmap
     (loop [cpt 0, n1 (first v), n2 (second v), m hmap]
       (if (< cpt 2)
         (let [ens (get (get m n1) :neigh), res (conj ens n2)]
             (recur (inc cpt) n2 n1 (assoc m n1 {:neigh res})))
         m))))
 
-;; Create a graph without the neigh
+;; Create a graph without the neigh (with le list of value)
 (defn makeGraph [lines]
   "Returns a hashmap contating the graph without t"
-  (loop [v (reduce concat (map intVect lines)), hmap {}]
+  (loop [v lines, hmap {}]
     (if (seq v)
       (if (contains? hmap (first v))
         (recur (rest v) hmap)
@@ -43,4 +43,13 @@
         res)))
 
 (defn erdos-renyi-rnd [n,p]
-  "Returns a G_{n,p} random graph, also known as an Erdős-Rényi graph")
+  "Returns a G_{n,p} random graph, also known as an Erdős-Rényi graph"
+  (loop [g (makeGraph (into [] (range n))), cpt 0, noeud (into [] (range n))]
+    (if (seq noeud)
+      (if (< cpt n)
+        ( let [rnd (rand)]
+          (if (and (not (= cpt (first noeud))) (<= rnd p))
+            (recur (link (conj []  cpt (first noeud)) g)  (inc cpt) noeud)
+            (recur g (inc cpt) noeud)))
+        (recur g 0 (rest noeud)))
+      g)))
